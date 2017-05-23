9d336533e5160dfd7e08b47eb32926e0c496f23a
niyazi <niyazi@niyazi.local>
2017-05-23 17:59:14 +0300

    first

---
+++ CellTableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  CellTableViewCell.swift
+//  Portfolyo JSON
+//
+//  Created by niyazi on 23/05/2017.
+//  Copyright © 2017 tanyildiz. All rights reserved.
+//
+
+import UIKit
+
+class CellTableViewCell: UITableViewCell {
+
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- Portfolyo JSON/ViewController.swift
+++ Portfolyo JSON/ViewController.swift
@@ -8,18 +8,54 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDataSource,UITableViewDelegate {
+    var works = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let urlString = "http://www.tanyildiz.com/?json=1"
+        let url = URL(string: urlString)
+        let task = URLSession.shared.dataTask(with: url!)
+        { (data, response, error) in
+            if error != nil
+            {
+                    print(error!)
+                } else {
+                        do {
+                            let json = try JSONSerialization.jsonObject(with: data!, options: .mutableContainers) as? NSDictionary
+                            if let jsonDic = json {
+                                for i in 0..<jsonDic.count {
+                                    if let workTitles = jsonDic[i] as? NSDictionary {
+                                        self.works.append(workTitles[self.works] as! NSString as String)
+                                                                                }
+                                                        }
+                                print(self.works)
+                                                }
+
+                            }
+                        catch {
+                            print(error)
+                              }
+                        }
+                }; task.resume()
+            }
+    
+    
+        override func didReceiveMemoryWarning() {
+            super.didReceiveMemoryWarning()
+            // Dispose of any resources that can be recreated.
+        }
+    
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return works.count
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        
     }
-
-
 }
+
 
